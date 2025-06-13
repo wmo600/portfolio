7e8d7be3834bef90e51e9b027b368c34cab8d9da
Jack-Wmo-Student <wmoo.23@ichat.sp.edu.sg>
2025-06-13 19:49:51 +0800

    updated contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,27 @@
+import '../styles/contact.css';
 import { motion } from 'framer-motion';
 
 const Contact = () => (
-  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+  <motion.div
+    className="contact-section"
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+  >
     <h1>$ contact --me</h1>
-    <p>📧 win@example.com</p>
+    <p>
+      email: <a href="mailto:Jack.Win.tech@proton.me">Jack.Win.tech@proton.me</a>
+    </p>
+    <p>
+      LinkedIn:{" "}
+      <a
+        href="https://www.linkedin.com/in/winmoeoo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        www.linkedin.com/in/winmoeoo
+      </a>
+    </p>
   </motion.div>
 );
 
